4c284d61083a4a4e945b2cdc4b48b4478b3b28f2
DongJun Kim <djkim35417@gmail.com>
2024-10-23 20:57:37 +0900

    Time: 3 ms (99.21%), Space: 42.6 MB (12.97%) - LeetHub

--- 0055-jump-game/0055-jump-game.kt
+++ 0055-jump-game/0055-jump-game.kt
@@ -1,6 +1,5 @@
 class Solution {
     var nums = intArrayOf()
-    var dp = intArrayOf()
     fun canJump(nums: IntArray): Boolean {
         initialize(nums)
         return solve()
@@ -8,35 +7,19 @@ class Solution {
 
     fun initialize(nums: IntArray) {
         this.nums = nums
-        this.dp = IntArray(nums.size) {0}
     }
 
     fun solve(): Boolean {
-        return dfs(0)
+        var gas = 0
+        for (n in nums) {
+            if (gas < 0) {
+                return false
+            } else if(n > gas) {
+                gas = n
+            }
+            gas -= 1
+        }
+        return true
     }
 
-    fun dfs(cur: Int): Boolean {
-        if (cur == nums.lastIndex) {
-            return true
-        }
-        if (dp[cur] != 0) {
-            if (dp[cur] == 1) {
-                return true
-            } else {
-                return false
-            }
-        }
-        var result = false
-        for (d in 1..nums[cur]) {
-            if (cur + d <= nums.lastIndex) {
-                result = result || dfs(cur+d)
-            }
-        }
-        if (result == true) {
-            dp[cur] = 1
-        } else {
-            dp[cur] = -1
-        }
-        return result
-    }
 }
